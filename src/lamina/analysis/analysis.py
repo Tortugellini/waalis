08c491dfbd0751dccc33a651e9b4c29884f62d04
Tortugellini <collin.crites@gmail.com>
2025-06-12 09:09:17 -0700

    Created analysis class.

---
+++ src/lamina/analysis/analysis.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# from skimage import
+
+
+class Discerna:
+    """A class used to analyze images."""
+
+    def __init__(self):
+        """
+        Instantiates a 'Discerna' object.
+        """
+
+        pass
